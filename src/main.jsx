0778b5de0e542917a9747c82d2c215477df9251c
Shhemeem <mohdshemeemkuzhiyengal@gmail.com>
2025-12-31 20:26:11 +0530

    Fix router and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <--- Added missing import
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* The Router must wrap the App inside the render function */}
+    <BrowserRouter basename="/Mileage_tracker">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
